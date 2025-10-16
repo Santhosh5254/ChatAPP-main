987c116ad31b69a32b3287555d5341313c787093
Nelluri Santhosh Kumar <160124616+Santhosh5254@users.noreply.github.com>
2025-10-16 23:58:13 +0530

    Refactor server port configuration and logging

--- ChatAPP-main/backend/server.js
+++ ChatAPP-main/backend/server.js
@@ -51,12 +51,14 @@ app.use("/api/messages",messageRouter);
 //connect MongoDB 
 await connectDB();
 
-if(process.env.NODE_ENV!=="production"){
-  const PORT = process.env.PORT|| 4000;
-server.listen(PORT,()=> console.log("server is running on this port : "+PORT));
-}
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
 
 export default server;
+
 /*
 import express from "express";
 import "dotenv/config";
